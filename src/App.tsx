c32fa001009901e4943cbca698dbe1da2cc15f27
marcusungarette <marcusungarette@gmail.com>
2021-11-16 21:55:40 -0300

    testing new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-//
+
 function App() {
   return (
     <div className="App">
@@ -22,5 +22,5 @@ function App() {
     </div>
   )
 }
-
+//
 export default App
